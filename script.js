6e13925c641158b6de706a1ef3e517770db79a7a
AryanChoudharyy <140917288+AryanChoudharyy@users.noreply.github.com>
2024-02-07 23:48:20 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -5,73 +5,67 @@ require('dotenv').config()
 
 const port = process.env.PORT || 3000
 
-var request = require('request');
-const bodyParser = require('body-parser');
-var multer = require('multer');
-const { resolveConfig } = require('vite');
-var upload = multer();
+var request = require('request')
+const bodyParser = require('body-parser')
+var multer = require('multer')
+const { resolveConfig } = require('vite')
+var upload = multer()
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/public'))
+app.set('view engine', 'ejs')
+app.use(upload.array())
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/public'));
-app.set('view engine', 'ejs');
-app.use(upload.array());
+let mData = ''
+let coinName = 'bitcoin'
+let mChart = ''
 
+async function resData (coinName) {
+  var marketData = await new Promise((resolve, reject) => {
+    request(
+      'https://api.coingecko.com/api/v3/coins/' + coinName,
+      function (error, response, body) {
+        console.error('error:', error) // Print the error if one occurred
+        console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
+        console.log('body:', typeof body) // Print the HTML for the Google homepage.
+        mData = JSON.parse(body)
+        resolve(mData)
+      }
+    )
+  })
 
-let mData = ""
-let coinName = "bitcoin"
-let mChart = ""
-
-async function resData(coinName){
-  var marketData = await new Promise((resolve,reject)=>{
-
-  
-    request('https://api.coingecko.com/api/v3/coins/'+ coinName, function (error, response, body) {
-      console.error('error:', error); // Print the error if one occurred
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-      console.log('body:', typeof body); // Print the HTML for the Google homepage.
-      mData = JSON.parse(body)
-    resolve(mData)
-  
-    });
-})
-
-   if(marketData){
-   var marketChart = await new Promise((resolve,reject)=>{
-
-  
-    request('https://api.coingecko.com/api/v3/coins/'+ coinName + '/market_chart?vs_currency=usd&days=30', function (error, response, body) {
-      console.error('error:', error); // Print the error if one occurred
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-      console.log('body:', typeof body); // Print the HTML for the Google homepage.
-      mChart = JSON.parse(body)
-      //console.log(mchart)
-    resolve(mData)
-    
-    });
-})
-   }
+  if (marketData) {
+    var marketChart = await new Promise((resolve, reject) => {
+      request(
+        'https://api.coingecko.com/api/v3/coins/' +
+          coinName +
+          '/market_chart?vs_currency=usd&days=30',
+        function (error, response, body) {
+          console.log({body})
+          console.error('error:', error) // Print the error if one occurred
+          console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
+          console.log('body:', typeof body) // Print the HTML for the Google homepage.
+          mChart = JSON.parse(body)
+          //console.log(mchart)
+          resolve(mData)
+        }
+      )
+    })
+  }
 }
 
-
-
-
-
-
-app.get('/', async(req, res) => {
+app.get('/', async (req, res) => {
   await resData(coinName)
-  res.render('index',{mData,mChart} )
+  res.render('index', { mData, mChart })
 })
 
-
-app.post('/', async(req, res) => {
-  coinName = req.body.selectCoin; 
+app.post('/', async (req, res) => {
+  coinName = req.body.selectCoin
   await resData(coinName)
-  res.render('index',{mData,mChart} )
+  res.render('index', { mData, mChart })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on https://localhost:${port}`)
+  console.log(`Example app listening on localhost:${port}`)
 })
-
