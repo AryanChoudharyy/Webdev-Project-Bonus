97ebe092a3dec9c162c21af599810faae31ef191
AryanChoudharyy <140917288+AryanChoudharyy@users.noreply.github.com>
2024-02-08 00:13:43 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -21,40 +21,53 @@ let mData = ''
 let coinName = 'bitcoin'
 let mChart = ''
 
-async function resData (coinName) {
+async function resData(coinName) {
+  // Set custom headers
+  const headers = {
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
+    "X-Requested-With": "XMLHttpRequest"
+  };
+
+  // Request for market data
   var marketData = await new Promise((resolve, reject) => {
     request(
-      'https://api.coingecko.com/api/v3/coins/' + coinName,
+      {
+        url: 'https://api.coingecko.com/api/v3/coins/' + coinName,
+        headers: headers
+      },
       function (error, response, body) {
-        console.log({body})
-        console.error('error:', error) // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
-        console.log('body:', typeof body) // Print the HTML for the Google homepage.
-        mData = JSON.parse(body)
-        resolve(mData)
+        console.log({ body });
+        console.error('error:', error);
+        console.log('statusCode:', response && response.statusCode);
+        console.log('body:', typeof body);
+        mData = JSON.parse(body);
+        resolve(mData);
       }
-    )
-  })
+    );
+  });
 
+  // Check if marketData is available before making the second request
   if (marketData) {
+    // Request for market chart
     var marketChart = await new Promise((resolve, reject) => {
       request(
-        'https://api.coingecko.com/api/v3/coins/' +
-          coinName +
-          '/market_chart?vs_currency=usd&days=30',
+        {
+          url: 'https://api.coingecko.com/api/v3/coins/' + coinName + '/market_chart?vs_currency=usd&days=30',
+          headers: headers
+        },
         function (error, response, body) {
-          console.log({body})
-          console.error('error:', error) // Print the error if one occurred
-          console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
-          console.log('body:', typeof body) // Print the HTML for the Google homepage.
-          mChart = JSON.parse(body)
-          //console.log(mchart)
-          resolve(mData)
+          console.log({ body });
+          console.error('error:', error);
+          console.log('statusCode:', response && response.statusCode);
+          console.log('body:', typeof body);
+          mChart = JSON.parse(body);
+          resolve(mData);
         }
-      )
-    })
+      );
+    });
   }
 }
+
 
 app.get('/', async (req, res) => {
   await resData(coinName)
