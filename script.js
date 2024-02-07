35dec6d02d15781a8fccc6b894115cbaec56d53a
AryanChoudharyy <140917288+AryanChoudharyy@users.noreply.github.com>
2024-02-08 00:05:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ async function resData (coinName) {
     request(
       'https://api.coingecko.com/api/v3/coins/' + coinName,
       function (error, response, body) {
+        console.log({body})
         console.error('error:', error) // Print the error if one occurred
         console.log('statusCode:', response && response.statusCode) // Print the response status code if a response was received
         console.log('body:', typeof body) // Print the HTML for the Google homepage.
